65c3c1c3b5f5ac2e16b45b310eec31aec3c0b789
Tyson La <tysonala@hotmail.com>
2024-03-27 22:02:26 -0500

    Cart Controller

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,22 @@
+# app/models/cart.rb
 class Cart < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true  
+  has_many :cart_items, dependent: :destroy
+end
+
+# app/models/cart_item.rb
+class CartItem < ApplicationRecord
+  belongs_to :cart
+  belongs_to :product
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+end
+
+# app/models/user.rb
+class User < ApplicationRecord
+  has_one :cart
+end
+
+# app/models/product.rb
+class Product < ApplicationRecord
+  has_many :cart_items
 end
