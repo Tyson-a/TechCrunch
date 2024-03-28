47658d28853550a69e49056ae2598a27ee6ac62f
Tyson La <tysonala@hotmail.com>
2024-03-27 22:03:11 -0500

    CartItem Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      Cart.find(session[:cart_id])
+    else
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+  end
 end
